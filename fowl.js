6fd9777a83dc401d8b4f9c2c62dfeb9407887aa2
Axel Forsman <axelsfor@gmail.com>
2016-01-09 22:00:25 +0100

    Documented the API.

--- fowl.js
+++ fowl.js
@@ -1,13 +1,36 @@
-var fowl = (function() {
-	"use strict";
+var fowl = (function() { "use strict";
 	var INITIAL_SIZE = 1024;
 	var componentCount = 0;
+	/**
+	 * An entity manager.
+	 * @constructor
+	 */
 	var EntityManager = function() {
+		/**
+		 * Array of all components associated with entities.
+		 * @type {Object[]}
+		 */
 		this.components = new Array(componentCount * INITIAL_SIZE);
+		/**
+		 * Array of bitflags for each entity's component.
+		 * @type {Uint32Array}
+		 */
 		this.entityMask = new Uint32Array(INITIAL_SIZE);
+		/**
+		 * Pool of availible entity identifiers.
+		 * @type {number[]}
+		 */
 		this.pool = [];
 	};
+	/**
+	 * The highest identifier that has been given to an entity.
+	 * @type {number}
+	 */
 	EntityManager.prototype.count = 0;
+	/**
+	 * Create a new entity.
+	 * @return {number} The identifier of the new entity.
+	 */
 	EntityManager.prototype.createEntity = function() {
 		var entity = this.pool.length > 0 ? this.pool.pop() : this.count++;
 		if (entity >= this.entityMask.length) {
@@ -16,25 +39,54 @@ var fowl = (function() {
 		}
 		return entity;
 	};
+	/**
+	 * Remove the entity.
+	 * @param {number} entity - The identifier of the entity to remove.
+	 */
 	EntityManager.prototype.removeEntity = function(entity) {
 		this.entityMask[entity] = 0;
 		this.pool.push(entity);
 	};
+	/**
+	 * Assign a component to an entity.
+	 * @param {number} entity - The entity.
+	 * @param {?Object} component - The component.
+	 * @return {?Object} The component.
+	 */
 	EntityManager.prototype.addComponent = function(entity, component) {
 		var key = component.constructor.componentId;
 		this.entityMask[entity] |= 1 << key;
 		return this.components[componentCount * entity + key] = component;
 	};
+	/**
+	 * Remove a component from an entity.
+	 * @param {number} entity - The entity.
+	 * @param {!Object} - The component.
+	 */
 	EntityManager.prototype.removeComponent = function(entity, component) {
 		this.entityMask[entity] &= ~(1 << component.componentId);
 	};
-	EntityManager.prototype.getComponent = function(entity, key) {
-		return this.components[componentCount * entity + key.componentId];
+	/**
+	 * Retrieve a component from an entity.
+	 * @param {number} entity - The entity.
+	 * @param {!Object} component - The component.
+	 * @return {?Object} The component from the entity.
+	 */
+	EntityManager.prototype.getComponent = function(entity, component) {
+		return this.components[componentCount * entity + component.componentId];
 	};
+	/**
+	 * Remove all entities.
+	 */
 	EntityManager.prototype.clear = function() {
 		this.entityMask.fill(0);
 		this.components = [];
 	};
+	/**
+	 * Applies the callback to each entity with the specified components.
+	 * @param {function(number)} callback - The callback.
+	 * @param {...Object} Components that the entities mush have.
+	 */
 	EntityManager.prototype.each = function(callback) {
 		var mask = 0;
 		for (var i = 1; i < arguments.length; ++i) {
@@ -48,7 +100,11 @@ var fowl = (function() {
 	};
 	return {
 		EntityManager: EntityManager,
-			registerComponents: function(components) {
+			/**
+			 * Register the components. Must be called before instantiating EntityManager. Sets the property componentId on the supplied components.
+			 * @param {...Object} var_args - The components.
+			 */
+			registerComponents: function() {
 				if (arguments.length > 32) throw new RangeError("Too many components");
 				for (var i = 0, length = arguments.length; i < length; ++i) {
 					arguments[i].componentId = i;
